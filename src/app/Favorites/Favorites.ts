b53d7c1b077b7727f2d2aea6b106401871851b8e
maxancespg <maxance.spg@gmail.com>
2026-01-18 19:35:40 +0100

    ajout de la page favoris

--- src/app/Favorites/Favorites.ts
+++ src/app/Favorites/Favorites.ts
@@ -1,9 +1,35 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Item } from '../../lib/types';
+import { AuthService } from '../auth.service';
+import { getUserFavorites } from '../../lib/api';
+import { Card } from '../../Components/Card/Card';
 
 @Component({
   selector: 'app-favorites',
-  imports: [],
+  imports: [Card],
   templateUrl: './Favorites.html',
   styleUrl: './Favorites.css'
 })
-export class Favorites {}
+export class Favorites implements OnInit {
+  constructor(private auth: AuthService) {}
+
+  items: Item[] = [];
+
+  ngOnInit() {
+    // Vérifier si l'utilisateur est authentifié
+    if (!this.auth.isAuthenticated()) {
+      console.error('User is not authenticated');
+      return;
+    }
+
+
+    // Récupérer les films favoris de l'utilisateur
+    getUserFavorites(this.auth.getToken()!)
+      .then((data) => {
+        this.items = data;
+      })
+      .catch((error) => {
+        console.error('Error fetching user favorites:', error);
+      });
+  }
+}
